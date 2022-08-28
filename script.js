dd02795d89ecf00a2ff9b095035a983a722822a2
Ashim Poudel <poudelashim26@gmail.com>
2022-08-29 09:18:06 +1000

    manipulating css with DOM

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ let score = 20;
 
 document.querySelector('.number').textContent = secretNumber;
 
-//game logic//
+//game logic
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number !';
+    document.querySelector('body').style.background = '#E6816B ';
   } else if (guess === secretNumber) {
+    //when player wins the game
     document.querySelector('.message').textContent =
       '🥳 Well done,  Correct Number';
+    document.querySelector('body').style.background = '#60b347';
+
+    //when player inputs high number
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -24,6 +30,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
+    //when input is too low
     if (score > 1) {
       document.querySelector('.message').textContent = '📉Too Low!';
       score--;
