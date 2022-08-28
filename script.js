154e0d2253c760db5b25d7efc2a7c635cd2d6a6f
Ashim Poudel <poudelashim26@gmail.com>
2022-08-29 08:59:33 +1000

    implemented game logic

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+//defining secret number//
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+//sate variable//
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+//game logic//
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number !';
+    document.querySelector('.message').textContent = '⛔ No Number !';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🥳 Well done,  Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
