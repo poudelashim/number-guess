6269f362146aced83e886418dc94af64cb10cd29
Ashim Poudel <poudelashim26@gmail.com>
2022-08-29 08:20:52 +1000

    inititated event handler

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'correct number';
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number !';
+  }
+});
